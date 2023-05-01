7011042ec360538f9b1171e23bc07f2b2afe5c6f
maxxberg <max.git@mx-berg.de>
2023-05-01 22:18:25 +0000

    added userparameters

--- roles/zabbix_agent/files/scripts/wireguard/get-iptables.sh
+++ roles/zabbix_agent/files/scripts/wireguard/get-iptables.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-var1=$(iptables -L "$1" | grep "$2")
-[ -n "${var1}" ] && echo True || echo False
+if ! systemctl is-active --quiet firewalld.service; then
+    var1=$(iptables -L "$1" | grep "$2")
+    [ -n "$var1" ] && echo True || echo False
+fi
